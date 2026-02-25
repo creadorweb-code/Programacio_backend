fa235c73254f5415c09a347a59154abc47404caa
Hugo sanchez <sanchez_hugo20@hotmail.com>
2026-02-24 21:59:32 -0500

    Se agrega modulosNPM

---
+++ TemaUno_Lenguajes/Ejercicios_1.3/modulosNPM/index.js
@@ -0,0 +1,14 @@
+
+
+//var generateName = require('sillyname');
+
+//var sillyName = generateName();
+
+//console.log(sillyName); 
+
+
+const { randomSuperhero } = require('superheroes');
+
+const superhero = randomSuperhero();
+
+console.log(superhero);
