780973fa7f7c66169242c1af091c1376e1bbe945
Hugo sanchez <sanchez_hugo20@hotmail.com>
2026-03-04 17:19:16 -0500

    Añadiendo actividad 1.3 moduleNode

--- TemaUno_Lenguajes/Ejercicios_1.3/moduloNode/index.js
+++ TemaUno_Lenguajes/Ejercicios_1.3/moduloNode/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');  // Importa la herramienta de sistema de archivos...
 
-fs.writeFile('archivo.txt', 'Hola desde NodeJS', (err) => {
+fs.writeFile('archivo.txt', 'Hola desde NodeJS.', (err) => {
     if (err) throw err;
-    console.log('El archivo ha sido creado con éxito.');
+    console.log('El archivo ha sido creado con éxito..');
 
 
  // 2️⃣ Leer el archivo después de crearlo.
