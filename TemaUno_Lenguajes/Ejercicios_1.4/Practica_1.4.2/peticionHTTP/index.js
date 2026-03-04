7bf438ebee632e08b494271dc53ad963df36bc52
Hugo sanchez <sanchez_hugo20@hotmail.com>
2026-03-04 17:43:56 -0500

    Añadiendo actividad Practica 1.4.2

---
+++ TemaUno_Lenguajes/Ejercicios_1.4/Practica_1.4.2/peticionHTTP/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+// Ruta GET inicial
+app.get('/', (req, res) => {
+    res.send('<h1>Hola Mundo</h1>');
+    res.sendStatus(200);
+});
+
+// Método POST - Registro
+app.post('/registro', (req, res) => {
+     res.sendStatus(201);
+});
+
+// Método PUT - Actualizar usuario
+app.put('/usuario/actualizar', (req, res) => {
+    res.sendStatus(200);
+});
+
+
+// Método PATCH - Modificar usuario
+app.patch('/usuario/modificar', (req, res) => {
+    res.sendStatus(200)
+});
+
+// Método DELETE - Eliminar usuario
+app.delete('/usuario/eliminar', (req, res) => {
+    res.sendStatus(200)
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`);
+});
