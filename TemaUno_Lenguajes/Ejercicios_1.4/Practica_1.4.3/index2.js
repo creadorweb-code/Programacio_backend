4677f0093157b8e6a0c8a728739c09f836a0d282
Hugo sanchez <sanchez_hugo20@hotmail.com>
2026-03-06 02:30:29 -0500

    Agrego nueva carpeta practica_1.4.3

---
+++ TemaUno_Lenguajes/Ejercicios_1.4/Practica_1.4.3/index2.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+console.log(__dirname);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+
+  const nombreEquipo = req.body.mascota + " " + req.body.adjetivo;
+
+  res.send("Nombre del equipo: " + nombreEquipo);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor ejecutándose en el puerto ${port}`);
+});
+
